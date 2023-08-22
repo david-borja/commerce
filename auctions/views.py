c9c5507fcda184e4d597d282b088bbbc5e53b8dd
david-borja <davidborjamartinez@gmail.com>
2023-08-22 07:56:41 +0200

    add: create view

--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,6 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def create(request):
+    return render(request, "auctions/create.html")
