cb501c4f712c6c891341bdb2d8e10dd3449ba639
david-borja <davidborjamartinez@gmail.com>
2023-10-22 12:14:20 +0200

    refactor: created reusable input error-input and submit-button components

---
+++ auctions/templatetags/custom_tags.py
@@ -0,0 +1,26 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('auctions/components/input-group.html')
+def custom_input(name, type, placeholder, label):
+    return {
+        'name': name,
+        'type': type,
+        'placeholder': placeholder,
+        'label': label
+    }
+
+@register.inclusion_tag('auctions/components/error-input-group.html')
+def custom_error_input(name, type, placeholder, label):
+    return {
+        'name': name,
+        'type': type,
+        'placeholder': placeholder,
+        'label': label
+    }
+
+@register.inclusion_tag('auctions/components/submit-button.html')
+def custom_submit_button(text):
+    return {
+        'text': text,
+    }
