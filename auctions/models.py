9faf63759c1f2d9b0fc2da6072724b372df69ce6
david-borja <davidborjamartinez@gmail.com>
2024-02-04 18:33:16 +0100

    fix: update models

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 # User
 class User(AbstractUser):
@@ -7,15 +8,16 @@ class User(AbstractUser):
 
 # Listing
 class Listing(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=128)
     starting_bid = models.FloatField()
-    highest_bid_id = models.ForeignKey("Bid", blank=True, default=None, on_delete=models.CASCADE,  related_name="lisitings_won") # string references ( "Bid" ) can be use for referencing models that are defined below
-    image = models.CharField(max_length=128, blank=True)
+    highest_bid_id = models.ForeignKey("Bid", blank=True, null=True,  on_delete=models.CASCADE,  related_name="lisitings_won") # string references ( "Bid" ) can be use for referencing models that are defined below
+    image_url = models.CharField(max_length=128, blank=True)
     category = models.CharField(max_length=64, blank=True)
     is_active = models.BooleanField(default=True)
-    saved_by = models.ManyToManyField(User, blank=True, related_name="watchlist")
-    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="won_auctions")
+    saved_by = models.ManyToManyField(User, blank=True, null=True, related_name="watchlist")
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="won_auctions", blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="published_listings")
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -24,6 +26,7 @@ class Listing(models.Model):
 
 # Bid
 class Bid(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
     price = models.FloatField()
@@ -34,6 +37,7 @@ class Bid(models.Model):
 
 # Comments
 class Comments(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(max_length=128)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comments")
@@ -44,4 +48,5 @@ class Comments(models.Model):
 
 # Categories
 class Categories(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64)
