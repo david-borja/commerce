d51ed0856ec043dd7c4438ef0ab7b89c1dca0a6e
david-borja <davidborjamartinez@gmail.com>
2024-02-04 18:34:02 +0100

    chore: configure admin app

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bid, Listing, User, Comments, Categories
+
+class BidAdmin(admin.ModelAdmin):
+  list_display = ("id", "listing", "user", "price", "created_at")
+
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ("id", "title", "description", "starting_bid", "highest_bid_id", "image_url", "category", "is_active", "winner", "author", "created_at")
+  filter_horizontal = ("saved_by",)
+
+class UserAdmin(admin.ModelAdmin):
+  list_display = ("username", "email", "password")
+
+class CommentsAdmin(admin.ModelAdmin):
+  list_display = ("id", "text", "user", "listing", "created_at")
+
+class CategoriesAdmin(admin.ModelAdmin):
+  list_display = ("id", "name")
+
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Categories, CategoriesAdmin)
