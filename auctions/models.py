53c8e20bbe1725da48fc7ba0bc001fd4e4ff4b02
david-borja <davidborjamartinez@gmail.com>
2024-08-14 15:15:02 +0200

    feat(): add category names to admin view

--- auctions/models.py
+++ auctions/models.py
@@ -85,3 +85,6 @@ class Category(models.Model):
     icons = models.CharField(max_length=64, blank=True)
     class Meta:
         verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
